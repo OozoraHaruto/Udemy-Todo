918bbe67934485789c942a9578b0a0fe93c59bed
Malcolm Chew <malcolm_chibi@hotmail.com>
2017-06-03 18:43:16 +0800

    Add filter functionality

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -48,12 +48,13 @@ class TodoApp extends React.Component{
   }
 
   render(){
-    var {todos} = this.state;
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
     return(
       <div className="controls">
         <TodoSearch onSearch={this.handleSearch} />
-        <TodoList todos={todos} onToggle={this.handleToggle}/>
+        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddTodo} />
       </div>
     )
